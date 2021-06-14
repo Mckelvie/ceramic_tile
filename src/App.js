51fdc46b2ab93a5b8929a9e08f6fce50355d8def
mamazhanov <book.sites9@gmail.com>
2021-06-14 16:46:03 +0600

    fuck

--- src/App.js
+++ src/App.js
@@ -24,29 +24,36 @@ class App extends Component {
     render() {
         return (
             <div style={{margin: "0 150px"}}>
-                <div className="bg-light py-2 mb-2" style={{borderRadius: "0 0 5px 5px", position: "sticky", top: "0", zIndex: 2, boxShadow: "0 0 15px rgba(0,0,0,0.5)"}}>
-                    <Container className="row d-flex mb-1 align-items-center justify-content-between" style={{position: 'sticky', top: 0}}>
-                        <div  className="row align-items-center">
-                            <img className={"mx-5"} style={{width: 50,  borderRadius: "50%"}}
-                                 src={"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png"}/>
-                            <h3>Lorem ipsum dolor</h3>
+                <div className="bg-light py-2 mb-2" style={{
+                    borderRadius: "0 0 5px 5px",
+                    position: "sticky",
+                    top: "0",
+                    zIndex: 2,
+                    boxShadow: "0 0 15px rgba(0,0,0,0.5)"
+                }}>
+                    <Container className="row d-flex mb-1 align-items-center justify-content-between"
+                               style={{position: 'sticky', top: 0}}>
+                        <div className="row align-items-center">
+                            <NavLink to="/"  style={{textDecoration: "none", color: "black"}}>
+                                <h1 style={{fontFamily: "Poppins", fontWeight: 900, marginLeft: 50}}>CeramicTile</h1>
+                            </NavLink>
                         </div>
                         <div className="d-flex row align-items-flex-end">
 
-                                <NavLink to="/admin" >
-                                    <Button
-                                        className="mr-2 ml-2"
-                                        variant="contained"
-                                        color="primary"
-                                    >добавить товар</Button>
-                                </NavLink>
+                            <NavLink to="/admin">
+                                <Button
+                                    className="mr-2 ml-2"
+                                    variant="contained"
+                                    color="primary"
+                                >добавить товар</Button>
+                            </NavLink>
 
-                            <NavLink to="/history" >
+                            <NavLink to="/history">
                                 <Button
                                     variant="contained"
                                     color="primary"
                                 >История заказов</Button>
-                                </NavLink>
+                            </NavLink>
 
 
                         </div>
@@ -63,14 +70,18 @@ class App extends Component {
                                </div>}/>
 
                     <Route path='/admin'
-                           render={() => <Add_product/> }/>
+                           render={() => <Add_product/>}/>
 
                     <Route path='/history'
-                           render={() => <History/> }/>
+                           render={() => <History/>}/>
 
                 </Container>
 
-                <div className="bg-light d-flex align-items-center justify-content-center" style={{borderRadius: "5px 5px 0 0", marginTop: 15, height: 100, position: "sticky", bottom: 0, left: 0, right: 0}}>
+                <div className="bg-light d-flex align-items-center justify-content-center" style={{
+                    borderRadius: "5px 5px 0 0",
+                    marginTop: 15,
+                    height: 100,
+                }}>
                     <h1> © Lorem ipsum dolor</h1>
                 </div>
             </div>
--- src/components/Add_product.js
+++ src/components/Add_product.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
-
+import firebase from "firebase";
 
 class MyComponent extends Component {
+
     render() {
+        const inputs = {
+            width: 250,
+            fontsize: 25
+        }
+        const div_inp = { width: 400, display: "flex", justifyContent: "space-between", margin: "10px 0"}
         return (
             <div>
-                <div className="d-flex align-items-center justify-content-center">
-                    <div>
-                        <label>Name</label>
-                        <input type="text"/>
+                <div className="d-flex flex-column align-items-center justify-content-center">
+                    <div style={div_inp}>
+                        <label>Название плитки:</label>
+                        <input style={inputs} type="text"/>
+                    </div>
+                    <div style={div_inp}>
+                        <label>Количество плитки:</label>
+                        <input style={inputs} type="number"/>
+                    </div>
+                    <div style={div_inp}>
+                        <label>Цена плитки:</label>
+                        <input style={inputs} type="number"/>
+                    </div>
+                    <div style={div_inp}>
+                        <label>Картинка плитки:</label>
+                        <input style={inputs} type="file"/>
                     </div>
                 </div>
             </div>
@@ -16,6 +34,5 @@ class MyComponent extends Component {
     }
 }
 
-MyComponent.propTypes = {};
 
 export default MyComponent;
