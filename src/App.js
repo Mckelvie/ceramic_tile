335b8a60db9a58525f6a81ec91340a86bc1e6550
mamazhanov <book.sites9@gmail.com>
2021-06-15 00:36:53 +0600

    uh

--- src/App.js
+++ src/App.js
@@ -4,87 +4,106 @@ import Sales from "./components/Sales";
 import React, {Component} from "react";
 import data from "./data.json";
 import {Button, Container} from "@material-ui/core";
-import SendIcon from "@material-ui/icons/Send";
 import {NavLink, Route} from 'react-router-dom';
 import Add_product from "./components/Add_product";
 import History from "./components/history";
+import firebase from "./firebase";
+
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            warehouse: data.warehouse
+            warehouse: data.warehouse,
+            products: [],
+            load: false
         }
     }
 
     componentDidMount() {
-        console.log(this.state.warehouse)
+        firebase.firestore().collection('products').get().then(s =>{
+            this.setState({
+                products: s.docs.map(v => {
+                    return v.data();
+                })
+            })
+        });
+        console.log(this.state.products);
+    }
+
+    fix = () => {
+        console.log(this.state.products)
     }
 
     render() {
         return (
-            <div style={{margin: "0 150px"}}>
-                <div className="bg-light py-2 mb-2" style={{
-                    borderRadius: "0 0 5px 5px",
-                    position: "sticky",
-                    top: "0",
-                    zIndex: 2,
-                    boxShadow: "0 0 15px rgba(0,0,0,0.5)"
-                }}>
-                    <Container className="row d-flex mb-1 align-items-center justify-content-between"
-                               style={{position: 'sticky', top: 0}}>
-                        <div className="row align-items-center">
-                            <NavLink to="/"  style={{textDecoration: "none", color: "black"}}>
-                                <h1 style={{fontFamily: "Poppins", fontWeight: 900, marginLeft: 50}}>CeramicTile</h1>
-                            </NavLink>
-                        </div>
-                        <div className="d-flex row align-items-flex-end">
+           <div>
+               {
+                   this.state.load ? "Loading!" : <div style={{margin: "0 150px"}}>
+                       <div className="bg-light py-2 mb-2" style={{
+                           borderRadius: "0 0 5px 5px",
+                           position: "sticky",
+                           top: "0",
+                           zIndex: 2,
+                           boxShadow: "0 0 15px rgba(0,0,0,0.5)"
+                       }}>
+                           <Container className="row d-flex mb-1 align-items-center justify-content-between"
+                                      style={{position: 'sticky', top: 0}}>
+                               <div className="row align-items-center">
+                                   <NavLink to="/"  style={{textDecoration: "none", color: "black"}}>
+                                       <h1 style={{fontFamily: "Poppins", fontWeight: 900, marginLeft: 50}}>CeramicTile</h1>
+                                   </NavLink>
+                               </div>
+                               <div className="d-flex row align-items-flex-end">
 
-                            <NavLink to="/admin">
-                                <Button
-                                    className="mr-2 ml-2"
-                                    variant="contained"
-                                    color="primary"
-                                >добавить товар</Button>
-                            </NavLink>
+                                   <NavLink to="/admin">
+                                       <Button
+                                           className="mr-2 ml-2"
+                                           variant="contained"
+                                           color="primary"
+                                       >добавить товар</Button>
+                                   </NavLink>
 
-                            <NavLink to="/history">
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                >История заказов</Button>
-                            </NavLink>
+                                   <NavLink to="/history">
+                                       <Button
+                                           variant="contained"
+                                           color="primary"
+                                       >История заказов</Button>
+                                   </NavLink>
 
 
-                        </div>
-                    </Container>
-                </div>
+                               </div>
+                           </Container>
+                       </div>
 
-                <Container className="App" style={{backgroundColor: '#eee', padding: 4, borderRadius: 10}}>
+                       <Container className="App" style={{backgroundColor: '#eee', padding: 4, borderRadius: 10}}>
 
-                    <Route path='/' exact
-                           render={() =>
-                               <div>
-                                   <Dashboard/>
-                                   <Sales warehouse={this.state.warehouse}/>
-                               </div>}/>
+                           <Route path='/' exact
+                                  render={() =>
+                                      <div>
+                                          <Dashboard/>
+                                          <Sales warehouse={this.state.warehouse} products={this.state.products}/>
+                                      </div>}/>
 
-                    <Route path='/admin'
-                           render={() => <Add_product/>}/>
+                           <Route path='/admin'
+                                  render={() => <Add_product/>}/>
 
-                    <Route path='/history'
-                           render={() => <History/>}/>
+                           <Route path='/history'
+                                  render={() => <History/>}/>
 
-                </Container>
+                       </Container>
 
-                <div className="bg-light d-flex align-items-center justify-content-center" style={{
-                    borderRadius: "5px 5px 0 0",
-                    marginTop: 15,
-                    height: 100,
-                }}>
-                    <h1> © Lorem ipsum dolor</h1>
-                </div>
-            </div>
+                       <div className="bg-light d-flex align-items-center justify-content-center" style={{
+                           borderRadius: "5px 5px 0 0",
+                           marginTop: 15,
+                           height: 100,
+                       }}>
+                           <h1> © Lorem ipsum dolor</h1>
+                       </div>
+
+                   </div>
+               }
+           </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import { StrictMode } from "react";
 
 ReactDOM.render(
   <BrowserRouter>
-      <App />
+      <StrictMode>
+        <App />
+      </StrictMode>
   </BrowserRouter>,
   document.getElementById('root')
 );
